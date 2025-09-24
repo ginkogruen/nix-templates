aee99bce9a74aacb1018db898ee173432238a99b
ginkogruen <jasper@wolter-koeln.de>
2025-09-24 14:15:08 +0200

    flake: Added systems for flake-parts

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } (_: {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+
       flake.templates = {
         gleam.path = ./templates/gleam;
         gleam.description = "Gleam + nix-gleam";
